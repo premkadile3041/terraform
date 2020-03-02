20928399a07774808b2aa4b250d620393e8a0c53
premkadile3041 <prem.kadile@gmail.com>
2020-03-02 22:44:45 +0530

    added terraform file

---
+++ pager.tf
@@ -0,0 +1,30 @@
+provider "pagerduty" {
+  token = "RpMFfTZS9qgRyGy-fAyY"
+}
+
+# Create a PagerDuty team
+resource "pagerduty_team" "engineering" {
+  name        = "Engineering"
+  description = "All engineering"
+}
+
+# Create a PagerDuty user
+resource "pagerduty_user" "sudeep" {
+  name  = "SudeepM"
+  email = "sudeep.malepati@ibexlabs.com"
+  role  = "admin"
+}
+resource "pagerduty_escalation_policy" "pipeline" {
+  name      = "Data Pipeline Escalation Policy test"
+  num_loops = 5
+
+  rule {
+    escalation_delay_in_minutes = 15
+
+    target {
+      type = "user_reference"
+      id   = pagerduty_user.sudeep.id
+    }
+  }
+}
+
